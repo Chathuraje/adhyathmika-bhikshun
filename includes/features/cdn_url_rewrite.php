7a8c0ec3a347475b36a12b3ce2b3c01fdbd0ade5
Chathura J Ekanayake <chathuraje01@gmail.com>
2025-07-16 19:51:03 +0530

    Refactor CDN URL rewrite function: Add S3 URL handling in filters for attachment URLs

--- includes/features/cdn_url_rewrite.php
+++ includes/features/cdn_url_rewrite.php
@@ -1,14 +1,14 @@
 <?php
-/**
- * Enable CDN URL Rewrite Filters
- */
 function ab_enable_cdn_url_rewrite() {
     $cdn_url = get_option('ab_cdn_url', '');
     $site_url = get_site_url();
+    $s3_url = 'https://s3.eu-west-2.amazonaws.com/cdn.adhyathmikabhikshun.org';
 
     if ($cdn_url) {
-        add_filter('as3cf_get_attachment_url', function ($url) use ($cdn_url, $site_url) {
-            return str_replace($site_url, $cdn_url, $url);
+        add_filter('as3cf_get_attachment_url', function ($url) use ($cdn_url, $site_url, $s3_url) {
+            $url = str_replace($s3_url, $cdn_url, $url);
+            $url = str_replace($site_url, $cdn_url, $url);
+            return $url;
         });
 
         add_filter('wp_get_attachment_url', function ($url) use ($cdn_url, $site_url) {
@@ -16,3 +16,4 @@ function ab_enable_cdn_url_rewrite() {
         });
     }
 }
+?>

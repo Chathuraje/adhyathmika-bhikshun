e1a7c3c4b5e9eab0aaae768518869dd6258ff468
Chathura J Ekanayake <chathuraje01@gmail.com>
2025-07-16 20:00:54 +0530

    Refactor CDN URL rewrite function: Simplify attachment URL filter by removing redundant site URL replacement

--- includes/features/cdn_url_rewrite.php
+++ includes/features/cdn_url_rewrite.php
@@ -5,10 +5,8 @@ function ab_enable_cdn_url_rewrite() {
     $s3_url = 'https://s3.eu-west-2.amazonaws.com/cdn.adhyathmikabhikshun.org';
 
     if ($cdn_url) {
-        add_filter('as3cf_get_attachment_url', function ($url) use ($cdn_url, $site_url, $s3_url) {
-            $url = str_replace($s3_url, $cdn_url, $url);
-            $url = str_replace($site_url, $cdn_url, $url);
-            return $url;
+        add_filter('as3cf_get_attachment_url', function ($url) use ($cdn_url, $s3_url) {
+            return str_replace($s3_url, $cdn_url, $url);
         });
 
         add_filter('wp_get_attachment_url', function ($url) use ($cdn_url, $site_url) {

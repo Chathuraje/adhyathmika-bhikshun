1304cf95532a6f1542ed216c7a8b89827afc8347
Chathura J Ekanayake <chathuraje01@gmail.com>
2025-07-17 22:46:40 +0530

    Comment out unused export/import manager page function in main.php and related includes in admin.php

--- includes/admin.php
+++ includes/admin.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . '/functions/admin.php';
 require_once __DIR__ . '/post-managements/admin.php';
 
-require_once __DIR__ . '/export-import/site_contents/admin.php';
-require_once __DIR__ . '/export-import/custom_posts/admin.php';
-require_once __DIR__ . '/export-import/all_posts/admin.php';
+// require_once __DIR__ . '/export-import/site_contents/admin.php';
+// require_once __DIR__ . '/export-import/custom_posts/admin.php';
+// require_once __DIR__ . '/export-import/all_posts/admin.php';
 
--- pages/main.php
+++ pages/main.php
@@ -5,11 +5,11 @@ function abh_dashboard()
     require_once plugin_dir_path(__FILE__) . '/dashboard-page.php';
 }
 
-// Load unified export/import manager page
-function abh_export_import_manager_page()
-{
-    require_once plugin_dir_path(__FILE__) . '/export-import-manager-page.php';
-}
+// // Load unified export/import manager page
+// function abh_export_import_manager_page()
+// {
+//     require_once plugin_dir_path(__FILE__) . '/export-import-manager-page.php';
+// }
 
 // Load feature page
 function abh_features_page()

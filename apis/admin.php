7afe0819b8dd39780c149660501aa1f64354b462
Chathura J Ekanayake <chathuraje01@gmail.com>
2025-07-19 01:04:01 +0530

    Add conditional inclusion for sync-post.php based on Airtable sync option

--- apis/admin.php
+++ apis/admin.php
@@ -13,4 +13,8 @@ if (get_option('ab_import_posts_to_site_enabled', true)) {
     include_once __DIR__ . '/import-posts.php';
 }
 
+if (get_option('ab_sync_posts_to_airtable_enabled', true)) {
+    include_once __DIR__ . '/sync-post.php';
+}
+
 ?>
